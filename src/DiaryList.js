e57a90fa1941ddebd23212e18f715dfc56e02a65
yxxh020 <yiran023h@gmail.com>
2023-09-12 09:29:17 +0900

    리스트 렌더링 / 일기 리스트, 아이템 추가

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,9 +1,26 @@
-const DiaryList = () => {
+import DiaryItem from "./DiaryItem";
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
   return (
     <div className="DiaryList">
-      <h2>일기 리스트</h2>
+      <h2>diary list</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it, idx) => (
+          <DiaryItem key={it.id} {...it} />
+          /* 
+        key prop.배열안에서 항목을 고유식별하기위해 사용함.
+        map의 idx도 사용가능(순서가 바뀔 수 있는 경우에는 X) 
+        key와 일기 데이터를 prop으로 모두 전달
+        */
+        ))}
+      </div>
     </div>
   );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
 };
 
 export default DiaryList;
